ce316e9a015f4be06e0dcb523b78ae001d64df1b
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-06-09 15:50:39 -0700

    added get and set free endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var free = false;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/setFree', function(req, res, next) {
+    if (typeof req.body.free === 'undefined' || req.body.free === null) {
+        res.send(400);
+    } else {
+        if (req.body.free === true) {
+            free = true;
+            res.send(200);
+        } else if (req.body.free === false) {
+            free = false;
+            res.send(200);
+        } else {
+            res.send(400);
+        }
+    }
+});
+
+router.get('/isFree', function(req, res, next) {
+    res.send(free);
+})
 
 module.exports = router;
